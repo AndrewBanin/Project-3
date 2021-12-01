d36a2bcc67da13ef587428a858af5d36ca7aa437
banina <andybanin@gmail.com>
2021-12-01 04:29:05 -0500

    a

---
+++ one.java
@@ -0,0 +1,25 @@
+Analysis
+personal bankingprivillage privatewealth
+
+
+Requirement 
+Analysis
+designing
+coding
+testing
+delivery
+monitoring
+waterfull 
+agile or incremental
+java---\
+oracle database--------->------>email----------> operations
+----------
+git init
+git config --global user.name "banina"
+git config --global user.email "andybanin@gmail.com"
+git status
+git add
+git add .
+git commit -m "first commit"
+git log
+git log --oneline
